4df366b3ecd8c291bb11a2cc9e43b0d71b1b70e7
Michael Guo <michaelgguo@gmail.com>
2018-12-06 14:21:15 -0600

    procesed csv & nltk

--- parser.py
+++ parser.py
@@ -1,10 +1,19 @@
 import csv
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
+import nltk
 
-with open('data/UScomments.csv') as csvfile:
-    readCSV = pandas.io.parsers.read_csv(csvfile)
-    data = []
-    for row in readCSV:
-        print(row)
-        data.append(row)
+# with open('data/UScomments.csv') as csvfile:
+#     data = pd.read_csv(csvfile, error_bad_lines=False)
+#     data.to_csv("data/processed.csv")
+
+data = pd.read_csv("data/processed.csv")
+
+print(data.head())
+
+
+sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+
+tokens = nltk.word_tokenize(sentence)
+
+print(tokens)
